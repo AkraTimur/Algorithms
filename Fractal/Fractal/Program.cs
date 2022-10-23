add14f874b8cadab146ad0686e328ccc5ef1747d
AKRA <99009898+AkraTimur@users.noreply.github.com>
2022-10-23 10:30:17 +0300

    add new algorithm

---
+++ Fractal/Fractal/Program.cs
@@ -0,0 +1,22 @@
+﻿namespace Fractal
+{
+    public static class Program
+    {
+        public static ulong Factorial(int n)
+        {
+            if (n == 0) return 1;
+            else return (ulong)n * Factorial(n - 1);
+
+        }
+        static void Main(string[] args)
+        {
+            Console.WriteLine(Factorial(0));
+            Console.WriteLine(Factorial(1));
+            Console.WriteLine(Factorial(2));
+            Console.WriteLine(Factorial(3));
+            Console.WriteLine(Factorial(4));
+            Console.WriteLine(Factorial(5));
+
+        }
+    }
+}
