b36592bf076ddaab4ece6422c8faa65d6be01a9b
AKRA <timur.akra@outlook.com>
2023-01-18 20:07:08 +0300

    add new algo

---
+++ Log2n/Program.cs
@@ -0,0 +1,15 @@
+﻿namespace Log2n
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            Log2n(8);
+        }
+        public static void Log2n(int n)
+        {
+            double log = Math.Log(n) / Math.Log(2);
+            Console.WriteLine($"log2({n}) = {log}");
+        }
+    }   
+}
